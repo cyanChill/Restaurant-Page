ecf025e5c4d6d79f8387f4db98e24c1bfab60c8f
cyanchill <anthonyliang9@gmail.com>
2021-08-25 18:42:49 -0400

    create navbar using individual elements and applied eventlistener to them

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,37 @@
+import displayHomePage from "./home.js";
+import displayMenuPage from "./menu.js";
+import displayContactPage from "./contact.js";
+import kebabCasefy from "./utility.js";
+
 export default function createNav() {
   const navContainer = document.createElement("div");
   navContainer.classList.add("nav-container");
 
-  navContainer.innerHTML = `
-    <nav id="nav-bar">
-      <a class="nav-link" data-page="home">Home</a>
-      <a class="nav-link" data-page="menu">Menu</a>
-      <a class="nav-link" data-page="contact">Contact</a>
-    </nav>
-  `;
+  const navbar = document.createElement("nav");
+  navbar.id = "nav-bar";
+  navContainer.appendChild(navbar);
+
+  const homeLink = createNavLink("Home");
+  navbar.appendChild(homeLink);
+  homeLink.addEventListener("click", displayHomePage);
+
+  const menuLink = createNavLink("Menu");
+  navbar.appendChild(menuLink);
+  menuLink.addEventListener("click", displayMenuPage);
+
+  const contactLink = createNavLink("Contact");
+  navbar.appendChild(contactLink);
+  contactLink.addEventListener("click", displayContactPage);
 
   return navContainer;
+}
+
+function createNavLink(content) {
+  const anchor = document.createElement("a");
+  anchor.textContent = content;
+  anchor.classList.add("nav-link");
+  anchor.href = "#";
+  anchor.setAttribute("data-page", kebabCasefy(content));
+
+  return anchor;
 }
