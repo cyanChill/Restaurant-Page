8b3d9f8e67e2d2b2c3ef9e25d8c662843dc4fe0d
cyanchill <anthonyliang9@gmail.com>
2021-08-25 22:10:42 -0400

    add function to assist with custom tab indication animation

--- src/utility.js
+++ src/utility.js
@@ -2,4 +2,12 @@ function kebabCasefy(str) {
   return str.toLowerCase().replace(/\s/g, "-");
 }
 
-export { kebabCasefy };
+function selectCurrentPage(page) {
+  const currLink = kebabCasefy(page);
+  const navLinks = document.querySelectorAll("a.nav-link");
+  navLinks.forEach((link) => link.classList.remove("selected"));
+  const currentPage = document.querySelector(`a[data-page="${currLink}"]`);
+  currentPage.classList.add("selected");
+}
+
+export { kebabCasefy, selectCurrentPage };
